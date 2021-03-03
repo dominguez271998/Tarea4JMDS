a3fdbcc077444f6b879f770f3117ca9ff4b81a93
Jose Manuel <Jose Manuel@DESKTOP-QT7O9GT>
2021-03-03 22:45:22 +0100

    Renombra los parámetros del constructor de la clase para darle también un nombre más significativo.

--- Tarea4JMDS/src/Fecha/Fecha.java
+++ Tarea4JMDS/src/Fecha/Fecha.java
@@ -10,10 +10,10 @@ public class Fecha {
 	public int mes;
 	public int ano;
 
-	public Fecha(int d, int m, int a) {
-		this.dia = d;
-		this.mes = m;
-		this.ano = a;
+	public Fecha(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}
 
 	public boolean valida() {
